69fe76dbcce445d1998283a03dea24dbb0adace6
VEDICASHUTOSH <ashutosh.singh@vedicrishiastro.com>
2026-06-21 18:38:42 +0530

    defined app error

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,7 +1,9 @@
 import type { ErrorRequestHandler } from "express";
+import { AppError } from "../errors/app.error.js";
 
 export const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
+  const statusCode = err instanceof AppError ? err.statusCode : 500;
   return res
-    .status(500)
-    .json({ message: err.message || "Interna; server error" });
+    .status(statusCode)
+    .json({ message: err.message || "Internal server error" });
 };
