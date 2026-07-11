796d938d772bec134ef6eba41b4a23cde7774d30
Ashutoshbot1 <ashutosh21jh@gmail.com>
2026-07-11 17:19:22 +0530

    Added User auth type

---
+++ src/types/auth.types.ts
@@ -0,0 +1,14 @@
+export interface SignupBody {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  password_hash: string | null;
+  created_at: string;
+  updated_at: string;
+}
