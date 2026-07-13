8a0d3d2fbd63309b4b3b16d3289262886735f05a
Ashutoshbot1 <ashutosh21jh@gmail.com>
2026-07-13 12:46:00 +0530

    Return unauthorized errors for invalid auth tokens

--- src/errors/unauthorized.error.ts
+++ src/errors/unauthorized.error.ts
@@ -1,7 +1,7 @@
 import { AppError } from "./app.error.js";
 
 export class UnauthorizedError extends AppError {
-  constructor(message = "") {
+  constructor(message = "Unauthorized") {
     super(message, 401);
   }
 }
