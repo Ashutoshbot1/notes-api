38956fcb1fc5c60dffcadb231dd0b0005a29a51d
Ashutoshbot1 <ashutosh21jh@gmail.com>
2026-07-13 11:45:17 +0530

    Add authenticate middleware

---
+++ src/middlewares/authenticate.middleware.ts
@@ -0,0 +1,34 @@
+import type { RequestHandler } from "express";
+import { BadRequestError } from "../errors/bad-request.error.js";
+import jwt from "jsonwebtoken";
+import type { AccessTokenPayload } from "../types/auth.types.js";
+
+export const authenticate: RequestHandler = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    throw new BadRequestError("Authentication token missing");
+  }
+
+  if (!authHeader.startsWith("Bearer ")) {
+    throw new BadRequestError("Authentication token missing");
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  if (!token) {
+    throw new BadRequestError("Authentication token missing");
+  }
+
+  const secret = process.env.JWT_SECRET;
+
+  if (!secret) {
+    throw new Error("JWT_SECRET is missing");
+  }
+
+  const decoded = jwt.verify(token, secret) as AccessTokenPayload;
+
+  res.locals.authUser = decoded;
+
+  next();
+};
