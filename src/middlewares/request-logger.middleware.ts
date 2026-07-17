66a79ad418e5f1959a85e3181be1a389e204f591
Ashutoshbot1 <ashutosh21jh@gmail.com>
2026-07-17 16:59:19 +0530

    differ log type

--- src/middlewares/request-logger.middleware.ts
+++ src/middlewares/request-logger.middleware.ts
@@ -2,11 +2,16 @@ import type { RequestHandler } from "express";
 
 export const requestLogger: RequestHandler = (req, res, next) => {
   const startTime = Date.now();
+
   res.on("finish", () => {
     const duration = Date.now() - startTime;
-    console.log(
-      `${req.method} ${req.originalUrl} ${res.statusCode} ${duration}ms`,
-    );
+    const { method, originalUrl } = req;
+    const { statusCode } = res;
+    const logMessage = `${method} ${originalUrl} ${statusCode} ${duration}ms`;
+    if (statusCode >= 500) console.error(logMessage);
+    else if (statusCode >= 400) console.warn(logMessage);
+    else if (statusCode >= 300) console.info(logMessage);
+    else console.log(logMessage);
   });
   next();
 };
