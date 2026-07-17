bc43d496e6aae1a22f5d020fdd80ce5f395ca3c3
Ashutoshbot1 <ashutosh21jh@gmail.com>
2026-07-17 16:47:53 +0530

    Add request logging middleware

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,12 @@ import noteRouter from "./routes/note.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import { errorHandler } from "./middlewares/error.middleware.js";
 import { generalRateLimiter } from "./middlewares/rate-limit.middleware.js";
+import { requestLogger } from "./middlewares/request-logger.middleware.js";
 
 const app = express();
 
 // Middleware to parse JSON bodies
+app.use(requestLogger);
 app.use(express.json());
 app.use(generalRateLimiter);
 
