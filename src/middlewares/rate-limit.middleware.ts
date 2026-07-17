1c99066cc214325be5d7c4f717844431d7312cb9
Ashutoshbot1 <ashutosh21jh@gmail.com>
2026-07-17 15:48:31 +0530

    add rate-limit middleware.ts

---
+++ src/middlewares/rate-limit.middleware.ts
@@ -0,0 +1,21 @@
+import rateLimit from "express-rate-limit";
+
+export const authRateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 5,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    message: "Too many auth attempts, please try again later.",
+  },
+});
+
+export const generatRateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    message: "Too many requests, please try again later",
+  },
+});
